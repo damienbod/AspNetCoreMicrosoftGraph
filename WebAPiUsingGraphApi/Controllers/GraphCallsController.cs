4bdbbdcac3c910df9a04c19b05370315cc87596d
damienbod <damien_bod@hotmail.com>
2020-11-19 13:21:11 +0100

    adding graph API client

--- WebAPiUsingGraphApi/Controllers/GraphCallsController.cs
+++ WebAPiUsingGraphApi/Controllers/GraphCallsController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPiUsingGraphApi.Controllers
